3e61c8ada166045f66040a3517f04aa4d434d3e2
Evgeny Belousov <skva1er@yandex.ru>
2022-11-22 00:31:20 +0400

    Fix negative account details tests

--- cypress/tests/e2e/e2e-account-details.spec.js
+++ cypress/tests/e2e/e2e-account-details.spec.js
@@ -14,12 +14,6 @@ describe('Account details spec', () => {
     it('should see account details', function () {
         cy.login(users.testuser.username, users.testuser.password);
 
-        // cy.getBySel('user-onboarding-next').click();
-        // cy.getBySel('bankaccount-bankName-input').type('12345');
-        // cy.getBySel('bankaccount-routingNumber-input').type('123456789');
-        // cy.getBySel('bankaccount-accountNumber-input').type('123456789');
-        // cy.getBySel('bankaccount-submit').click();
-
         cy.getBySel('sidenav-user-settings').click();
         cy.getBySel('user-settings-firstName-input').should('have.value', users.testuser.firstName);
         cy.getBySel('user-settings-lastName-input').should('have.value',users.testuser.lastName);
@@ -27,20 +21,20 @@ describe('Account details spec', () => {
         cy.getBySel('user-settings-phoneNumber-input').should('have.value',users.testuser.phoneNumber);
     });
 
-    it('should see helper text and disabled save button for invalid phone', function () {
-        cy.login('ship', '12345678');
+    it('should see helper text and disabled save button for invalid email address', function () {
+        cy.login(users.testuser.username, users.testuser.password);
 
         cy.getBySel('sidenav-user-settings').click();
-        cy.getBySel('user-settings-email-input').type('invalidemail');
+        cy.getBySel('user-settings-email-input').clear().type('invalidemail');
         cy.get('[id="user-settings-email-input-helper-text"]').contains('Must contain a valid email address');
         cy.getBySel('user-settings-submit').should('be.disabled');
     });
 
     it('should see helper text and disabled save button for invalid phone', function () {
-        cy.login('ship', '12345678');
+        cy.login(users.testuser.username, users.testuser.password);
 
         cy.getBySel('sidenav-user-settings').click();
-        cy.getBySel('user-settings-phoneNumber-input').type('invalidphone');
+        cy.getBySel('user-settings-phoneNumber-input').clear().type('invalidphone');
         cy.get('[id="user-settings-phoneNumber-input-helper-text"]').contains('Phone number is not valid');
         cy.getBySel('user-settings-submit').should('be.disabled');
     });
@@ -53,6 +47,5 @@ describe('Account details spec', () => {
         cy.getBySel('user-settings-submit').click();
         cy.reload();
         cy.getBySel('user-settings-firstName-input').should('have.value','newname');
-
     });
 });
