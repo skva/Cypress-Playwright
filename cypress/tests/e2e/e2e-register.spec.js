4b79810750b50f4a9257e27513e33ead4ab683a7
Evgeny Belousov <skva1er@yandex.ru>
2022-11-21 21:19:55 +0400

    Add data from db

--- cypress/tests/e2e/e2e-register.spec.js
+++ cypress/tests/e2e/e2e-register.spec.js
@@ -13,29 +13,29 @@ describe('Register account spec', () => {
 
     it('should register new user', function () {
         // Create new user
-        cy.getBySel('signup-first-name').type('Jack');
-        cy.getBySel('signup-last-name').type('Wreck');
-        cy.getBySel('signup-username').type('ship');
-        cy.getBySel('signup-password').type('12345678');
-        cy.getBySel('signup-confirmPassword').type('12345678');
+        cy.getBySel('signup-first-name').type(users.newuser.firstName);
+        cy.getBySel('signup-last-name').type(users.newuser.lastName);
+        cy.getBySel('signup-username').type(users.newuser.username);
+        cy.getBySel('signup-password').type(users.newuser.password);
+        cy.getBySel('signup-confirmPassword').type(users.newuser.password);
         cy.getBySel('signup-submit').click();
 
         // Login created user and complete onboarding
-        cy.login('ship', '12345678');
+        cy.login(users.newuser.username, users.newuser.password);
         cy.getBySel('user-onboarding-dialog-title').should('exist');
         cy.getBySel('user-onboarding-dialog-content').should('exist');
         cy.getBySel('user-onboarding-next').click();
 
         // Create bank account
         cy.getBySel('user-onboarding-dialog-title').should('exist');
-        cy.fillBankAccountFields('12345', '123456789', '123456789')
+        cy.fillBankAccountFields(users.bankaccount.bankName, users.bankaccount.routingName, users.bankaccount.accountNumber)
 
         // Complete onboarding
         cy.getBySel('user-onboarding-dialog-title').should('exist');
         cy.getBySel('user-onboarding-dialog-content').should('exist');
         cy.getBySel('user-onboarding-next').click();
 
-        cy.getBySel('sidenav-user-full-name').should( 'contain','Jack W')
+        cy.getBySel('sidenav-user-full-name').should( 'contain',(users.newuser.firstName + " " + (Array.from(users.newuser.lastName)[0])));
     });
 
     it('should see helper text and disabled submit button for password less 4 symbols', function () {
@@ -50,11 +50,11 @@ describe('Register account spec', () => {
     });
 
     it('should see helper text and disabled submit button for password less 4 symbols', function () {
-        cy.getBySel('signup-first-name').type('Jack');
-        cy.getBySel('signup-last-name').type('Wreck');
-        cy.getBySel('signup-username').type('ship');
-        cy.getBySel('signup-password').type('12345678');
-        cy.getBySel('signup-confirmPassword').type('12345679');
+        cy.getBySel('signup-first-name').type(users.newuser.firstName);
+        cy.getBySel('signup-last-name').type(users.newuser.lastName);
+        cy.getBySel('signup-username').type(users.newuser.username);
+        cy.getBySel('signup-password').type('123');
+        cy.getBySel('signup-confirmPassword').type('123');
 
         cy.getBySel('signup-confirmPassword').contains('Password does not match');
         cy.getBySel('signup-submit').should('be.disabled');
