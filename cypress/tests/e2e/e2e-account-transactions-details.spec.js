e8a87b88ea1bae242f08d60075244b4ad9e44651
Evgeny Belousov <skva1er@yandex.ru>
2022-11-24 23:24:27 +0400

    Add account transactions details test

--- cypress/tests/e2e/e2e-account-transactions-details.spec.js
+++ cypress/tests/e2e/e2e-account-transactions-details.spec.js
@@ -29,4 +29,12 @@ describe('Account transactions details spec', () => {
         cy.getBySel('new-transaction-return-to-transactions').click();
         cy.get('li:nth-child(1)').contains(payload.testtransaction.amount)
     })
+
+    it.only('should see like after click on bell', function () {
+        cy.login(users.testuser.username, users.testuser.password);
+
+        cy.get('div:nth-child(1) > li').click();
+        cy.get('div:nth-child(2) > button').click({force: true});
+        cy.get('[data-test^=transaction-like-count-]').contains(1);
+    })
 });
