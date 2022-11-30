422f915fca8997957e3d80a269067147f97d6fee
Evgeny Belousov <skva1er@yandex.ru>
2022-11-30 23:36:53 +0400

    Add get bankaccounts pw api test

---
+++ playwright/tests/api/api-bank-accounts.spec.js
@@ -0,0 +1,61 @@
+import {expect, test} from "@playwright/test";
+import users from "../../../cypress/fixtures/users.json";
+import bankaccounts from "../../../cypress/fixtures/bankaccounts.json";
+
+test.describe('GET /bankaccounts', () => {
+    test.beforeEach(async ({request}) => {
+        await request.post('http://localhost:3002/login', {
+            data: {
+                type: "LOGIN",
+                username: "Katharina_Bernier",
+                password: "s3cret"
+            }
+        })
+    });
+
+    test("Get bankaccounts should respond with code 200 and correct response body", async ({ request}) => {
+        const response = await request.get(`http://localhost:3002/bankaccounts`, {
+            data: {
+                type: "LOGIN",
+                username: "Katharina_Bernier",
+                password: "s3cret"
+            },
+        });
+        expect(response.status()).toBe(200);
+        const body = JSON.parse(await response.text());
+        await expect(body.results[0]).toHaveProperty('id');
+        await expect(body.results[0]).toHaveProperty('uuid');
+        await expect(body.results[0]).toHaveProperty('userId');
+        await expect(body.results[0]).toHaveProperty('bankName');
+        await expect(body.results[0]).toHaveProperty('accountNumber');
+        await expect(body.results[0]).toHaveProperty('routingNumber');
+        await expect(body.results[0]).toHaveProperty('isDeleted');
+        await expect(body.results[0]).toHaveProperty('createdAt');
+        await expect(body.results[0]).toHaveProperty('modifiedAt');
+        // await body.results.each(async el => {
+        //     await expect(el).toHaveProperty('id')
+        // })
+
+    });
+
+    test("Should delete bank account", async ({ request}) => {
+        const delResponse = await request.delete(`http://localhost:3002/bankaccounts/${bankaccounts.bankAccount.bankAccountId}`, {
+            data: {
+                type: "LOGIN",
+                username: "Katharina_Bernier",
+                password: "s3cret"
+            },
+        });
+        expect(delResponse.status()).toBe(200);
+        const getResponse = await request.get(`http://localhost:3002/bankaccounts/${bankaccounts.bankAccount.bankAccountId}`, {
+            data: {
+                type: "LOGIN",
+                username: "Katharina_Bernier",
+                password: "s3cret"
+            },
+        });
+        expect(getResponse.status()).toBe(200);
+        const body = JSON.parse(await getResponse.text());
+        await expect(body.account).toHaveProperty('isDeleted', true);
+    });
+})
